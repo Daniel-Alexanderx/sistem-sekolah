d253c67e0c242e2271178d66f270b457e092ff53
Daniel-Alexanderx <danielhuang190@gmail.com>
2026-01-27 13:14:18 +0700

    wip : implement student controller

---
+++ app/controllers/StudentController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controllers;
+ 
+class StudentController
+{
+    public function index()
+   
+    {
+            echo '<h1>DAFTAR SISWA</h1>';
+            echo '<p>Menampilkan daftar siswa</p>';
+
+    }
+ 
+ 
+ 
+public function create()
+    {
+            echo '<h1>TAMBAH SISWA</h1>';
+            echo '<p>Menampilkan form tambah siswa</p>';
+    }
+ 
+}
