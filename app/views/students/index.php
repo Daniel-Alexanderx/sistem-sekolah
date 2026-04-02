0fa17cc2fe6065597cac09944227afb31b97f168
Daniel-Alexanderx <danielhuang190@gmail.com>
2026-04-02 13:26:19 +0700

    feat: implement student index page

--- app/views/students/index.php
+++ app/views/students/index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="/css/output.css">
 </head>
 
-<body class="min-h-screen flex flex-col">
+<body class="min-h-screen flex flex-col bg-gray-100">
     <!-- Header Start -->
     <header class="bg-blue-700 text-white">
         <div class="flex items-center justify-between container mx-auto p-4">
@@ -18,13 +18,49 @@
     </header>
     <!-- Header end -->
     <main class="container mx-auto grow">
-        <div class="mt-8">
+        <div class="mt-8 space-y-2 ">
+
             <!-- Card Header Start -->
-            <div class="p-4 shadow rounded-lg">
+            <div class="p-4 shadow rounded-lg bg-white">
                 <h1 class="text-2xl font-bold">Daftar Siswa</h1>
                 <p>Menampilkan Daftar Siswa Terdatar</p>
             </div>
             <!-- Card Header End -->
+
+
+            <!-- Card Body start -->
+
+            <div class="bg-white shadow rounded-lg">
+                <table class="w-full">
+                    <thead class="bg-blue-200">
+                        <tr>
+                            <th class="px-4 py-2 text-left">No</th>
+                            <th class="px-4 py-2 text-left">Nama</th>
+                            <th class="px-4 py-2 text-left">NIS</th>
+                            <th class="px-4 py-2 text-left">Kelas</th>
+                            <th class="px-4 py-2 text-left">No Telepon</th>
+                            <th class="px-4 py-2">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="px-4 py-2 text-left">1</td>
+                            <td class="px-4 py-2 text-left">Andi</td>
+                            <td class="px-4 py-2 text-left">1234</td>
+                            <td class="px-4 py-2 text-left">11 TKJ II</td>
+                            <td class="px-4 py-2 text-left">08123456789</td>
+                            <td class="px-4 py-2">
+                                <div class="flex justify-center items-center gap-4">
+                                    <a href="" class="text-green-500">Detail</a>
+                                    <a href="" class="text-yellow-500">Edit</a>
+                                    <a href="" class="text-red-500">Hapus</a>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- Card Body end -->
         </div>
     </main>
     <!-- Main Start -->
