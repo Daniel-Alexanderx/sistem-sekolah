c73ac67eb009c1f4511bed4e1fcebfc3d15cba6c
Daniel-Alexanderx <danielhuang190@gmail.com>
2026-04-16 13:34:24 +0700

    feat : implement student detail page

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -2,37 +2,44 @@
 namespace App\Controllers;
 require_once '../app/core/Controller.php';
 require_once '../app/models/Student.php';
- 
+
 use App\Core\Controller;
 use App\Models\Student;
-  
+
 class StudentController extends Controller
 {
-    public function index()
-    {
-     $studentModel = new Student();
-     $students = $studentModel->getStudents();
+     public function index()
+     {
+          $studentModel = new Student();
+          $students = $studentModel->getStudents();
 
-    $this->view('students.index', [
-     'students' => $students
-    ]);
+          $this->view('students.index', [
+               'students' => $students
+          ]);
 
-    }
-   
- 
-    public function create()
-   {
-        $this->view('students.create');
-   }
- 
-   public function show(string $id)
-   {
-        $this->view('students.show', ['id' => $id]);
-   }
- 
-    public function edit(string $id)
-    {
-        $this->view('students.edit', ['id' => $id])  ;
      }
- 
+
+
+     public function create()
+     {
+          $this->view('students.create');
+     }
+
+     public function show(string $id)
+     {
+          $id = intval($id);
+
+          $studentModel = new Student();
+          $student = $studentModel->getStudent($id);
+
+          $this->view('students.show', [
+               'student' => $student
+          ]);
+     }
+
+     public function edit(string $id)
+     {
+          $this->view('students.edit', ['id' => $id]);
+     }
+
 }
--- app/views/students/show.php
+++ app/views/students/show.php
@@ -1,53 +1,53 @@
- <div class="mt-8 space-y-2 ">
+<div class="mt-8 space-y-2 ">
 
+    <!-- Card Header Start -->
+    <div class="p-4 shadow rounded-lg bg-white">
+        <h1 class="text-2xl font-bold">Tambah siswa</h1>
+        <p>Menambahkan siswa ke dalam sistem</p>
+    </div>
+    <!-- card Body start -->
 
-            <!-- Card Header Start -->
+    <div class="bg-white p-4 mt-4 shadow-md rounded-lg p-4">
+        <form action="" class="grid grid-cols-2 gap-4">
+            <div class="space-y-2">
 
-            <div class="p-4 shadow rounded-lg bg-white">
-                <h1 class="text-2xl font-bold">Detail siswa</h1>
-                <p>Menampilkan detail siswa yang terdaftar</p>
-            </div>
-            <!-- Card Header End -->
-
-
-            <!-- Card Body start -->
-
-            <div class="bg-white shadow rounded-lg p-4">
-
-                <div class="grid grid-cols-2 gap-4">
-
-                    <div class="space-y-2">
-                        <label class="block font-bold" for="name">Nama</label>
-                        <input class="w-full px-4 py-2 border rounded-lg" type="text" id="name"
-                            placeholder="Masukkan Nama" name="name" readonly>
-                    </div>
-
-                    <div class="space-y-2">
-                        <label class="block font-bold" for="nis">NIS</label>
-                        <input class="w-full px-4 py-2 border rounded-lg" type="text" id="nis"
-                            placeholder="Masukkan NIS" name="nis" readonly>
-                    </div>
-
-                    <div class="space-y-2">
-                        <label class="block font-bold" for="kelas">Kelas</label>
-                        <input class="w-full px-4 py-2 border rounded-lg" type="text" id="kelas"
-                            placeholder="Masukkan Kelas" name="kelas" readonly>
-                    </div>
-
-                    <div class="space-y-2">
-                        <label class="block font-bold" for="phone_number">Nomor Telepon</label>
-                        <input class="w-full px-4 py-2 border rounded-lg" type="text" id="phone_number"
-                            placeholder="Masukkan Nomor Telepon" name="phone_number" readonly>
-                    </div>
-
-                    <div class="flex justify-end col-span-2 gap-4">
-                        <a href="/students" class="py-2 px-4 bg-gray-100 rounded-lg text-blue-500">Kembali</a>
-                    </div>
-
-                </div>
-
+                <label class="block font-bold" for="name">Name</label>
+                <input value="<?= $student['name'] ?>" class="w-full px-4 py-2 border rounded-lg " type="text" id="name"
+                    placeholder="Masukkan nama" name="name" readonly>
             </div>
 
-            <!-- Card Body end -->
+            <div class="space-y-2">
 
-        </div> 
+                <label class="block font-bold" for="nis">Nis</label>
+                <input value="<?= $student['nis'] ?>" class="w-full px-4 py-2 border rounded-lg " type="text" id="nis"
+                    placeholder="Masukkan Nis" name="nis" readonly>
+            </div>
+
+            <div class="space-y-2">
+
+                <label class="block font-bold" for="kelas">Kelas</label>
+                <input value="<?= $student['class'] ?>" class="w-full px-4 py-2 border rounded-lg " type="text"
+                    id="kelas" placeholder="Masukkan kelas" name="kelas" readonly>
+            </div>
+
+            <div class="space-y-2">
+
+                <label class="block font-bold" for="telepon">Nomor Telepon</label>
+                <input value="<?= $student['phone_number'] ?>" class="w-full px-4 py-2 border rounded-lg " type="text"
+                    id="telepon" placeholder="Masukkan nomor telepon" name="telepon" readonly>
+            </div>
+
+            <div class="flex justify-end col-span-2 space-x-4 mt-4  ">
+
+                <a href="/students" class="py-2 px-4 bg-gray-100  rounded-lg">Kembali</a>
+
+            </div>
+        </form>
+    </div>
+
+</div>
+
+
+
+</div>
+</main>
